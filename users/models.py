4aad350b34e90c5375f9a2b1ffe0a9cb23f5727b
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-17 01:53:48 +0100

    Set Custom User Model To Inhert From AbstractUser
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
 
-class AuthorizedUser(models.Model):
+class AuthorizedUser(AbstractUser):
     """Custom user model class"""
     pass
