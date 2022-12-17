18554cce2272d24aedd3631c9ce23a772184267c
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-17 03:11:46 +0100

    Add Custom User Mode Manager
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from users.models import AuthorizedUser
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
 
