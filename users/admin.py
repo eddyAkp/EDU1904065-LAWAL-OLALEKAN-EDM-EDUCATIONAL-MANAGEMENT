aca670ebdcedb2f127c69931c741508d146ce990
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-17 01:56:05 +0100

    Make Custom User Viewable At Admin Site
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import AuthorizedUser
 
 # Register your models here.
+
+
+@admin.register(AuthorizedUser)
+class AuthorizedUserAdmin(UserAdmin): pass
