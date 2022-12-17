fbdf1245b62f1efbde478858a73ce96e74abfe2d
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-17 03:46:02 +0100

    Correct Wrong Spelling Of App/Directory Name 'resources' And Flesh Out Models
    'resources' models contains classes or representation of information
    material available in (abstract) inventory.
    
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/apps.py
+++ resources/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ResoursesConfig(AppConfig):
+class ResourcesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resourses'
+    name = 'resources'
---
+++ resources/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+MODE_OF_SERIALISATION: tuple = (
+    ("Episode", "Episode"),
+    ("Issue", "Issue"),
+)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, blank=False, null=False)
+
+
+class InformationMaterial(models.Model):
+    title = models.CharField(max_length=1000, blank=False, null=False)
+    quantity = models.PositiveIntegerField(default=1)
+    price_per_unit = models.PositiveIntegerField(default=1)
+    author = models.ForeignKey(Author)
+
+
+class AudioVisualMaterial(InformationMaterial):
+    pass
+
+
+class PrintMediaMaterial(InformationMaterial):
+    ISBN = models.CharField(max_length=30)
+    publish_date = models.DateField(blank=True)
+
+
+class SerialPrintMediaMaterial(PrintMediaMaterial):
+    serialization_mode = models.CharField(
+        choices=MODE_OF_SERIALISATION,
+        null=False, blank=False, max_length=100,
+    )
+    serial = models.CharField(max_length=100)
