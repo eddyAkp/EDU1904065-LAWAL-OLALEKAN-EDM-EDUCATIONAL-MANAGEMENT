b3946650575090cbabb93c94c72aa16283479b75
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-17 02:09:31 +0100

    Add Library Postiion Information Choices
    Required for creation of authorized personnel
    
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- users/models.py
+++ users/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+LIBRARY_POSITION_CHOICES = (
+    ('Chief Librarian', 'Chief Librarian'),
+    ('Head of Acquisitions', 'Head of Acquisitions')
+)
+
+
 class AuthorizedUser(AbstractUser):
     """Custom user model class"""
     pass
