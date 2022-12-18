758c8848c4401ded22c7a8beedfdf3a8caef7730
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-18 12:11:06 +0100

    Update Admin Site Dipslay For Resources
    Remove InformationMaterial from admin site as it is now deprecated to serve as base class.
    
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/admin.py
+++ resources/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
 from resources.models import (Author,
-                              InformationMaterial,
                               AudioVisualMaterial,
                               PrintMediaMaterial,
-                              SerialPrintMediaMaterial,
+                              SerialMaterial,
                               )
 
 # Register your models here.
@@ -17,11 +16,6 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ['name']
 
 
-@admin.register(InformationMaterial)
-class InformationMaterialAdmin(admin.ModelAdmin):
-    list_display = BASE_LIST_DISPLAY
-
-
 @admin.register(AudioVisualMaterial)
 class AudioVisualMaterialAdmin(admin.ModelAdmin): pass
 
@@ -31,6 +25,6 @@ class PrintMediaMaterialAdmin(admin.ModelAdmin):
     list_display = BASE_LIST_DISPLAY[:2] + ['ISBN'] + BASE_LIST_DISPLAY[2:] + ['publish_date']
 
 
-@admin.register(SerialPrintMediaMaterial)
-class SerialPrintMediaMaterialAdmin(admin.ModelAdmin):
+@admin.register(SerialMaterial)
+class SerialMaterialAdmin(admin.ModelAdmin):
     list_display = BASE_LIST_DISPLAY[:1] + ['serialization_display'] + BASE_LIST_DISPLAY[1:]
