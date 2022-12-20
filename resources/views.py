9887dbf8b0b5975dea20deb14e45d5ad7107667d
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-20 15:35:31 +0100

    Refactor Inventory Listing Queryset Generation
    Refactor method of combining querysets for all functional resource models.
    
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/views.py
+++ resources/views.py
@@ -1,7 +1,9 @@
+import itertools
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
-from resources.models import (AudioVisualMaterial, PrintMediaMaterial, SerialMaterial)
+from resources.models import (AudioVisualMaterial, PrintMediaMaterial)
 
 
 # Create your views here.
@@ -12,4 +14,10 @@ class InventoryListView(generic.ListView, LoginRequiredMixin):
     template_name = "inventory/inventory_list_view.html"
 
     def get_queryset(self):
-        return [AudioVisualMaterial.objects.all()] + [PrintMediaMaterial.objects.all()] + [PrintMediaMaterial.objects.all()]
+        return list(
+            itertools.chain(
+                AudioVisualMaterial.objects.all(),
+                PrintMediaMaterial.objects.all(),
+                PrintMediaMaterial.objects.all()
+            )
+        )
