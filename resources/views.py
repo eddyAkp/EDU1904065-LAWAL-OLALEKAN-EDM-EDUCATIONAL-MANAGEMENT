2f094306a0d5c7e2c1eacff3b6b77214640199a4
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-20 05:24:05 +0100

    Add Inventory ListView View
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/views.py
+++ resources/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+from resources.models import (AudioVisualMaterial, PrintMediaMaterial, SerialMaterial)
+
 
 # Create your views here.
+
+
+class InventoryListView(generic.ListView, LoginRequiredMixin):
+    context_object_name = "resources"
+    template_name = "inventory/inventory_list_view.html"
+
+    def get_queryset(self):
+        return [AudioVisualMaterial.objects.all()] + [PrintMediaMaterial.objects.all()] + [PrintMediaMaterial.objects.all()]
