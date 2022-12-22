a8bd04adac08c13fbf8561100efc2104afdfa730
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-22 09:39:48 +0100

    Add URLs For API End Point For Database Object Retrievals
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/urls.py
+++ resources/urls.py
@@ -4,4 +4,5 @@ import resources.views as resources_views
 
 urlpatterns = [
     path("", resources_views.InventoryListView.as_view()),
+    path("<str:resource_type>/<str:resource_id>/", resources_views.get_stock_count),
 ]
