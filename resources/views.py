b6211798c20086a2cb89c9a2cac809c0abf0387a
Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>
2022-12-22 09:40:53 +0100

    Add API End Point FBV For Rsource Models Data Retrieval
    Signed-off-by: Nwokolo Godwin <nwokolo.godwin.chidera@gmail.com>

--- resources/views.py
+++ resources/views.py
@@ -1,12 +1,35 @@
 import itertools
 
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
 from django.views import generic
 
-from resources.models import (AudioVisualMaterial, PrintMediaMaterial)
-
+from resources.models import (AudioVisualMaterial, Author, InformationMaterial, PrintMediaMaterial, SerialMaterial)
 
 # Create your views here.
+
+hashmap = {
+    "Author": Author,
+    "InformationMaterial": InformationMaterial,
+    "AudioVisualMaterial": AudioVisualMaterial,
+    "PrintMediaMaterial": PrintMediaMaterial,
+    "SerialMaterial": SerialMaterial,
+}
+
+
+def get_stock_count(request, resource_type: str, resource_id: str):
+    result = ""
+    try:
+        result = hashmap[resource_type].objects.get(id=int(resource_id))
+    except Exception as e:
+        print(e)
+    return JsonResponse(
+        {
+            "stock_count": result.quantity if result else "N/A",
+            "cost_per_unit": result.price_per_unit if result else "N/A",
+        },
+        safe=False
+    )
 
 
 class InventoryListView(generic.ListView, LoginRequiredMixin):
@@ -17,8 +40,8 @@ class InventoryListView(generic.ListView, LoginRequiredMixin):
     def get_queryset(self):
         return list(
             itertools.chain(
-                AudioVisualMaterial.objects.all(),
                 PrintMediaMaterial.objects.all(),
-                PrintMediaMaterial.objects.all()
+                PrintMediaMaterial.objects.all(),
+                AudioVisualMaterial.objects.all(),
             )
         )
